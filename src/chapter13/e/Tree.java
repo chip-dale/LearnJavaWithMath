07e58bf36c7ef6a32cda650ae754f3bda2c47970
Gadget Hackwrench <62562443+chip-dale@users.noreply.github.com>
2021-05-08 03:35:47 +0400

    Update Tree.java

--- src/chapter13/e/Tree.java
+++ src/chapter13/e/Tree.java
@@ -23,20 +23,20 @@ public class Tree {
 
 	public static void main(String[] args) {
 
-		int i = 0, j = 0, k = 0, num = 9;
+		int i = 0, j = 0, k = 0, dotCount = 9;
 		
-		for(i = 0; i <= num; i++) {
-			print(i, j, k, num);
+		for(i = 0; i <= dotCount; i++) {
+			print(i, j, k, dotCount);
 		}
-		for (i = num - (num - 2); i >= 0; i--) {
-			print(i, j, k, num);
+		for (i = dotCount - (dotCount - 2); i >= 0; i--) {
+			print(i, j, k, dotCount);
 		}
 		
 	}
 	
-	static void print(int i, int j, int k, int num) {
+	static void print(int i, int j, int k, int dotCount) {
 		if(i == 0) {
-			for(j = num - 1; j > i; j--) {
+			for(j = dotCount - 1; j > i; j--) {
 				System.out.print(' ');
 			}
 			for(k = 0; k < 2; k++) {
@@ -44,7 +44,7 @@ public class Tree {
 			}
 			System.out.println();
 		} else {
-			for(j = num - 1; j >= i; j--) {
+			for(j = dotCount - 1; j >= i; j--) {
 				System.out.print(' ');
 			}
 			for(k = 0; k < (i * 2); k++) {
